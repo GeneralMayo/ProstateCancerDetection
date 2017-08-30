ef4668a7954553a590724063098d06f47c6b9763
GeneralMayo <tmayosmith@gmail.com>
2017-08-30 14:31:26 -0400

    filling validation folders + new gitignore

--- preprocessing.py
+++ preprocessing.py
@@ -13,6 +13,7 @@ import skimage.measure as skm
 from matplotlib import pyplot as plt
 
 MAKE_DIRS = True
+PERCENT_TRAIN = .7
 
 # Image Sequence
 ImageSequence = ["T2tra", "T2sag", "ADC", "BVAL"]
@@ -103,231 +104,81 @@ with open(os.path.join(CSVPath, CSVImages), 'r') as CSV_images_object:
         #crop slices
         fullScanPixels = []
         for sliceNum in range(ArrayDicom.shape[2]):
-            if(abs(sliceNum - ijk[2]) <= ROISliceDistance):
+            if(abs(sliceNum - ijk[2]) <= ROISliceDistance):   
+                #get bounds
                 xStart = ijk[0]-int(ROISize[0]/2)
                 xFin = ijk[0]+int(ROISize[0]/2)
                 yStart = ijk[1]-int(ROISize[1]/2)
                 yFin = ijk[1]+int(ROISize[1]/2)
-                if(xStart>0 and yStart>0 and xFin<ConstPixelDims[0] and yFin<ConstPixelDims[1]):
-                    slicePixels = ArrayDicom[xStart:xFin,yStart:yFin,sliceNum]
+                if(ConstPixelDims[0]<ROISize[0] or ConstPixelDims[1]<ROISize[1]):
+                    raise NameError("ROI larger than image.")
+                if(xStart<0):
+                    xStart = 0
+                    xFin = ROISize[0]
+                elif(xFin>ConstPixelDims[0]):
+                    xStart = ConstPixelDims[0]-ROISize[0]
+                    xStart = ConstPixelDims[0]
+                if(yStart<0):
+                    yStart = 0
+                    yFin = ROISize[1]
+                elif(yFin>ConstPixelDims[1]):
+                    yStart = ConstPixelDims[1]-ROISize[1]
+                    yFin = ConstPixelDims[1]
 
-                    #save slice as png in appropriate folder
-                    if(imageType == 't2_tse_sag' or imageType == 't2_tse_tra'):
-                        imageTypeFolderName = imageType
-                    elif(imageType.split('_')[-1] == 'ADC'):
-                        imageTypeFolderName = 'ADC'
-                    elif(imageType.split('_')[-1] == 'BVAL'):
-                        imageTypeFolderName = 'BVAL'
-                    else:
-                        raise NameError('Image type issue should have been caught.')
+                #save slice as png in appropriate folder
+                slicePixels = ArrayDicom[xStart:xFin,yStart:yFin,sliceNum]
+                if(imageType == 't2_tse_sag' or imageType == 't2_tse_tra'):
+                    imageTypeFolderName = imageType
+                elif(imageType.split('_')[-1] == 'ADC'):
+                    imageTypeFolderName = 'ADC'
+                elif(imageType.split('_')[-1] == 'BVAL'):
+                    imageTypeFolderName = 'BVAL'
+                else:
+                    raise NameError('Image type issue should have been caught.')
 
-                    #get ggg
-                    ggg = findingsDict[row['ProxID']][row['fid']]
+                #get ggg
+                ggg = findingsDict[row['ProxID']][row['fid']]
 
-                    #save slice
-                    imagePath = os.path.join(ROIPath,'Train',imageTypeFolderName,str(ggg))
-                    imageName = row['ProxID']+'_'+imageTypeFolderName+'_'+row['fid']+'_'+str(sliceNum)
-                    print('Saved: '+imageName)
-                    scipy.misc.imsave(os.path.join(imagePath, imageName) + ".png", slicePixels)
+                #save slice
+                imagePath = os.path.join(ROIPath,'Train',imageTypeFolderName,str(ggg))
+                imageName = row['ProxID']+'_'+imageTypeFolderName+'_'+row['fid']+'_'+str(sliceNum)
+                print('Saved: '+imageName)
+                scipy.misc.imsave(os.path.join(imagePath, imageName) + ".png", slicePixels)
+                #fullScanPixels.append(slicePixels)
+
+"""
+os.mkdir(ROIPath+'/Train')
+    os.mkdir(ROIPath+'/Test')
+
+    imageTypes = ['ADC','BVAL','t2_tse_sag','t2_tse_tra']
+    testTrain = ['Test','Train']
+    for ttName in testTrain:
+        for imageType in imageTypes:
+            os.mkdir(ROIPath+'/'+ttName+'/'+imageType)
+            for ggg in range(5):
+                os.mkdir(ROIPath+'/'+ttName+'/'+imageType+'/'+str(ggg+1))
+"""
+
+print('Creating Test Set')
+#randomly move data to validation set
+for root, dirs, files in os.walk(ROIPath):
+    if(len(files)>0 and root.split('/')[2] != 'Test'):
+        random.shuffle(files)
+        splitPoint = int(round(len(files)*PERCENT_TRAIN))
+        toMove = files[splitPoint:]
+
+        #move data
+        for fileName in toMove:
+            src = root+'/'+fileName
+            dstFolders = src.split('/')
+            dstFolders[2] = 'Test'
+            dst = "/".join(dstFolders)
+            shutil.move(src,dst)
                     
-                    #fullScanPixels.append(slicePixels)
 
 
         #TODO test 3d CNN
         #fullScanPixels = np.array(fullScanPixels)
         #np.save(os.path.join(ROINumpyPath, sequence, 'ORI', image_filename), resized_ROI_image)
 
-        """
-        for i in range(ArrayDicom.shape[2]):
-
-
-
-
-        # Load spacing values (in mm)
-        ConstPixelSpacing = (float(RefDs.PixelSpacing[0]), float(RefDs.PixelSpacing[1]), float(RefDs.SliceThickness))
-        x = np.arange(0.0, (ConstPixelDims[0]+1)*ConstPixelSpacing[0], ConstPixelSpacing[0])
-        y = np.arange(0.0, (ConstPixelDims[1]+1)*ConstPixelSpacing[1], ConstPixelSpacing[1])
-        z = np.arange(0.0, (ConstPixelDims[2]+1)*ConstPixelSpacing[2], ConstPixelSpacing[2])
-
-
-        plt.figure(dpi=300)
-        plt.axes().set_aspect('equal', 'datalim')
-        plt.set_cmap(plt.gray())
-        plt.pcolormesh(x, y, np.flipud(ArrayDicom[:, :, 9]))
-        plt.show()
-        input()
-        """
-
-
-"""
-
-print("preprocessing start")
-start_time = time.time()
-
-# read csv file
-with open(os.path.join(CSVPath, CSVImages), 'r', newline='') as CSV_images_object:
-    # for each roi mask
-    for roi_file in os.listdir(ROIPath):
-        # parse roi filename
-        roi_filename = os.path.splitext(roi_file)[0]
-        roi_filename_split = roi_filename.split('_')
-
-        patient = roi_filename_split[0]
-        if len(roi_filename_split) == 4:
-            fid = int(roi_filename_split[1])
-        else:
-            CSV_images_object.seek(0)
-            CSV_images_reader = csv.DictReader(CSV_images_object)
-            for row in CSV_images_reader:
-                if row['ProxID'].split('-')[1] == patient:
-                    fid = int(row['fid'])
-                    break
-        sequence = roi_filename_split[len(roi_filename_split)-2]
-        slice = roi_filename_split[len(roi_filename_split)-1]
-
-        # skip DCE image
-        if sequence == 'kt':
-            continue
-        print(patient, fid, sequence, slice)
-
-        # read roi file
-        roi_object = read_roi_file(os.path.join(ROIPath, roi_file))
-        roi_temp = roi_object[roi_filename]
-
-        # find image dimension and path
-        CSV_images_object.seek(0)
-        CSV_images_reader = csv.DictReader(CSV_images_object)
-        for row in CSV_images_reader:
-            if row['ProxID'].split('-')[1] == patient:
-                if ((len(roi_filename_split) == 4) and (row['fid'] == str(fid))) or (len(roi_filename_split) == 3):
-                    if row['DCMSerDescr'] == sequence or \
-                            ((sequence == ImageSequence_ROI[0]) and (row['DCMSerDescr'] == ImageSequence[0])) or \
-                            ((sequence == ImageSequence_ROI[3]) and (row['DCMSerDescr'] == ImageSequence[3])):
-                        image_dimension = row['Dim'].split('x')
-                        image_path = row['DCMSerUID']
-
-                    '''
-                    # for ADC, DWI
-                    if sequence in [ImageSequence_ROI[2], ImageSequence_ROI[3]]:
-                        if row['DCMSerDescr'] == ImageSequence[2]:
-                            image_dimension = row['Dim'].split('x')
-                            image_path = row['DCMSerUID']
-                        elif row['DCMSerDescr'] == ImageSequence[3]:
-                            image_dimension = row['Dim'].split('x')
-                            image_path_DWI = row['DCMSerUID']
-                    # for T2
-                    else:
-                        if ((sequence == ImageSequence_ROI[0]) and (row['DCMSerDescr'] == ImageSequence[0]))\
-                                or ((sequence == ImageSequence_ROI[1]) and (row['DCMSerDescr'] == ImageSequence[1])):
-                            image_dimension = row['Dim'].split('x')
-                            image_path = row['DCMSerUID']
-                            break
-                    '''
-
-        # make polygon
-        polygon = []
-        for x, y in zip(roi_temp['x'], roi_temp['y']):
-            polygon.append(x)
-            polygon.append(y)
-
-        # make mask
-        width = int(image_dimension[0])
-        height = int(image_dimension[1])
-        img = Image.new('L', (width, height), 0)
-        ImageDraw.Draw(img).polygon(polygon, outline=1, fill=1)
-        mask = np.array(img)
-
-        # set dcm image path
-        patient_root = "ProstateX-" + patient
-        patient_root_path = os.path.join(DicomPath, patient_root)
-
-        for images in os.listdir(patient_root_path):
-            image_path = os.path.join(patient_root_path, images, image_path)
-
-            # Read dicom files
-            onlyfiles = [f for f in os.listdir(image_path) if os.path.isfile(os.path.join(image_path, f))]
-            for file in onlyfiles:
-                ds = dicom.read_file(os.path.join(image_path, file))
-
-                # get ROI image
-                slice_num = int(ds[0x20, 0x13].value)
-                if slice_num == int(slice):
-                    ROI_i0 = min(roi_temp['x'])
-                    ROI_i1 = max(roi_temp['x'])
-                    ROI_j0 = min(roi_temp['y'])
-                    ROI_j1 = max(roi_temp['y'])
-
-                    # apply mask
-                    masked_array = ds.pixel_array * mask
-                    ROI_image = masked_array[ROI_j0:ROI_j1, ROI_i0:ROI_i1]
-
-                    # resize ROi image
-                    if sequence in [ImageSequence_ROI[0], ImageSequence_ROI[1]]:
-                        dim = ROISize_T2
-                    else:
-                        dim = ROISize_ADC
-                        # sequence = ImageSequence_ROI[2]
-                    resized_ROI_image = cv2.resize(ROI_image, dim, interpolation=cv2.INTER_CUBIC)
-
-                    # display
-                    # plt.imshow(resized_ROI_image, cmap='gray')
-                    # plt.show()
-
-                    # save ROI image
-                    # as PNG
-                    image_filename = patient_root + "_" + str(fid) + "_" + str(sequence) + "_" + str(slice_num)
-                    scipy.misc.imsave(os.path.join(ROIImagePath, sequence, 'ORI', image_filename) + ".png", resized_ROI_image)
-
-                    # as npy
-                    np.save(os.path.join(ROINumpyPath, sequence, 'ORI', image_filename), resized_ROI_image)
-
-                    #
-                    # make textural feature map
-                    #
-
-                    # calculate textural feature for every voxel from glcm using 5x5 patch centered at each voxel
-                    feature_map = np.zeros(shape=(ROI_image.shape[0], ROI_image.shape[1], len(TexturalFeature)))
-                    y = 0
-                    for j in range(ROI_j0, ROI_j1):
-                        x = 0
-                        for i in range(ROI_i0, ROI_i1):
-                            ROI_image_texture = ds.pixel_array[j-2:j+2, i-2:i+2]
-
-                            ROI_image_uint8 = np.zeros(shape=ROI_image_texture.shape, dtype=np.uint8)
-                            ROI_image_uint8 = cv2.convertScaleAbs(ROI_image_texture, alpha=(255.0/65535.0))
-                            glcm = skf.greycomatrix(ROI_image_uint8, [1], [0], symmetric=True, normed=True)
-
-                            # make feature map
-                            idx = 0
-                            for feature in TexturalFeature:
-                                if feature == "entropy":
-                                    feature_map[y, x, idx] = skm.shannon_entropy(glcm)
-
-                                else:
-                                    feature_map[y, x, idx] = skf.greycoprops(glcm, feature)
-                                idx += 1
-
-                            x += 1
-                        y += 1
-
-                    # apply mask and resize
-                    masked_feature_map = np.zeros(shape=(ROI_image.shape[0], ROI_image.shape[1], len(TexturalFeature)))
-                    resized_feature_map = np.zeros(shape=(resized_ROI_image.shape[0], resized_ROI_image.shape[1], len(TexturalFeature)))
-                    ROI_mask = mask[ROI_j0:ROI_j1, ROI_i0:ROI_i1]
-                    idx = 0
-                    for feature in TexturalFeature:
-                        masked_feature_map[:, :, idx] = feature_map[:, :, idx] * ROI_mask
-                        resized_feature_map[:, :, idx] = cv2.resize(masked_feature_map[:, :, idx], dim, interpolation=cv2.INTER_CUBIC)
-                        idx += 1
-
-                    # save feature map image
-                    idx = 0
-                    for feature in TexturalFeature:
-                        # as PNG
-                        image_filename = patient_root + "_" + str(fid) + "_" + str(sequence) + "_" + feature + "_" + str(slice_num)
-                        scipy.misc.imsave(os.path.join(ROIImagePath, sequence, 'TF', image_filename) + ".png", resized_feature_map[:, :, idx])
-
-                        # as npy
-                        np.save(os.path.join(ROINumpyPath, sequence, 'TF', image_filename), resized_feature_map[:, :, idx])
-                        idx += 1
-"""
+        
